f7fd766f3b4f060317ab55bc2f4bfacb913e0a6e
AVRaspopov <81385947+AVRaspopov@users.noreply.github.com>
2021-04-25 22:37:12 +0300

    Initial commit

---
+++ src/main/java/com/game/repository/PlayerRepository.java
@@ -0,0 +1,12 @@
+package com.game.repository;
+
+import com.game.entity.Player;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PlayerRepository extends CrudRepository<Player, Long> {
+
+}
